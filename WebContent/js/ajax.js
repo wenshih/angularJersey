70a652361069e1caa755c61858e20d538db55dff
wen <wenshih99@gmail.com>
2015-07-24 17:43:40 +0800

    add spring

--- WebContent/js/ajax.js
+++ WebContent/js/ajax.js
@@ -1,7 +1,6 @@
 angular.module('stock').factory('ajaxApi',function ($http) {
 	
-	//var endpoint = "http://127.0.0.1:8080/jerseyStock/services";
-	var endpoint = "http://127.0.0.1:8080/aa/services";
+	var endpoint = "http://127.0.0.1:8080/angularJersey/services";
 	return {
 		getStockData: function(stockId) {
 			return $http({
